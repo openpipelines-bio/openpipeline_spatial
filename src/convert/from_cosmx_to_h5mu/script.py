00cafd63af52decfa15830ba20bc88f7a9384965
Dorien <41797896+dorien-er@users.noreply.github.com>
2025-07-10 15:46:25 +0200

    Update changelog and implement ci linting (#4)
    * update changelog
    
    * add openpipeline incubator as repository
    
    * add linting
    
    * lint
    
    * cleanup

--- src/convert/from_cosmx_to_h5mu/script.py
+++ src/convert/from_cosmx_to_h5mu/script.py
@@ -9,11 +9,9 @@ par = {
     "output": "./resources_test/cosmx/Lung5_Rep2_tiny.h5mu",
     "dataset_id": "Lung5_Rep2",
     "modality": "rna",
-    "output_compression": None
+    "output_compression": None,
 }
-meta ={
-    "resources_dir": "src/utils"
-}
+meta = {"resources_dir": "src/utils"}
 ## VIASH END
 
 sys.path.append(meta["resources_dir"])
@@ -26,15 +24,14 @@ fov_file = f"{par['dataset_id']}_fov_positions_file.csv"
 meta_file = f"{par['dataset_id']}_metadata_file.csv"
 
 for file in [counts_file, fov_file, meta_file]:
-    assert os.path.isfile(os.path.join(par["input"], file)), f"File does not exist: {file}"
+    assert os.path.isfile(os.path.join(par["input"], file)), (
+        f"File does not exist: {file}"
+    )
 
 logger.info("Reading in CosMx data...")
 adata = sq.read.nanostring(
-    path=par["input"],
-    counts_file=counts_file,
-    meta_file=meta_file,
-    fov_file=fov_file
-    )
+    path=par["input"], counts_file=counts_file, meta_file=meta_file, fov_file=fov_file
+)
 
 logger.info("Writing output MuData object...")
 mdata = mu.MuData({par["modality"]: adata})
--- src/convert/from_cosmx_to_h5mu/test.py
+++ src/convert/from_cosmx_to_h5mu/test.py
@@ -2,15 +2,20 @@ import pytest
 import sys
 import mudata as mu
 
+
 def test_simple_execution(run_component, tmp_path):
     output = tmp_path / "cosmx_tiny.h5mu"
 
     run_component(
         [
-            "--input", meta["resources_dir"] + "/Lung5_Rep2_tiny",
-            "--dataset_id", "Lung5_Rep2",
-            "--num_fovs", "2",
-            "--output", output,
+            "--input",
+            meta["resources_dir"] + "/Lung5_Rep2_tiny",
+            "--dataset_id",
+            "Lung5_Rep2",
+            "--num_fovs",
+            "2",
+            "--output",
+            output,
         ]
     )
     assert output.is_file(), "output file was not created"
@@ -21,25 +26,25 @@ def test_simple_execution(run_component, tmp_path):
     adata = mdata.mod["rna"]
 
     assert list(adata.obs.keys()) == [
-        'fov',
-        'Area',
-        'AspectRatio',
-        'CenterX_global_px',
-        'CenterY_global_px',
-        'Width',
-        'Height',
-        'Mean.MembraneStain',
-        'Max.MembraneStain',
-        'Mean.PanCK',
-        'Max.PanCK',
-        'Mean.CD45',
-        'Max.CD45',
-        'Mean.CD3',
-        'Max.CD3',
+        "fov",
+        "Area",
+        "AspectRatio",
+        "CenterX_global_px",
+        "CenterY_global_px",
+        "Width",
+        "Height",
+        "Mean.MembraneStain",
+        "Max.MembraneStain",
+        "Mean.PanCK",
+        "Max.PanCK",
+        "Mean.CD45",
+        "Max.CD45",
+        "Mean.CD3",
+        "Max.CD3",
         "Mean.DAPI",
         "Max.DAPI",
-        "cell_ID"
-        ]
+        "cell_ID",
+    ]
 
     assert list(adata.uns.keys()) == ["spatial"]
     assert list(adata.obsm.keys()) == ["spatial", "spatial_fov"]
