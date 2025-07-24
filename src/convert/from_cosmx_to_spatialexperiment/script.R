06cb0ba7b49209794c3f8f27883ef812bf770dfa
dorien-er <dorien-er@users.noreply.github.com>
2025-07-24 14:48:57 +0000

    deploy: 02ebcce04492923f928213e7311a0ff84fef04a7

---
+++ src/convert/from_cosmx_to_spatialexperiment/script.R
@@ -0,0 +1,34 @@
+library(SpatialExperimentIO)
+
+### VIASH START
+par <- list(
+  input = "resources_test/cosmx/Lung5_Rep2_tiny",
+  add_tx_path = TRUE,
+  add_polygon_path = FALSE,
+  add_fov_positions = TRUE,
+  alternative_experiment_features = c("NegPrb", "Negative", "SystemControl", "FalseCode"),
+  output = "spe_cosmx_test.rds"
+)
+### VIASH END
+
+if (par$add_polygon_path == FALSE & par$add_tx_path == FALSE) {
+  add_parquet_paths <- FALSE
+} else {
+  add_parquet_paths <- TRUE
+}
+
+spe <- readCosmxSXE(
+  dirName = par$input,
+  returnType = "SPE",
+  countMatPattern = "exprMat_file.csv",
+  metaDataPattern = "metadata_file.csv",
+  coordNames = c("CenterX_global_px", "CenterY_global_px"),
+  addFovPos = par$add_fov_positions,
+  fovPosPattern = "fov_positions_file.csv",
+  addParquetPaths = add_parquet_paths,
+  addPolygon = par$add_polygon_path,
+  addTx = par$add_tx_path,
+  altExps = par$alternative_experiment_features
+)
+
+saveRDS(spe, file = par$output)
---
+++ src/convert/from_xenium_to_spatialexperiment/test.R
@@ -0,0 +1,106 @@
+library(testthat, warn.conflicts = FALSE)
+library(SpatialExperimentIO)
+library(SpatialExperiment)
+
+## VIASH START
+meta <- list(
+  executable = "target/executable/convert/from_xenium_to_spatialexperiment/from_xenium_to_spatialexperiment",
+  resources_dir = "resources_test/xenium",
+  name = "from_xenium_to_spatial_experiment"
+)
+## VIASH END
+
+cat("> Checking simple execution\n")
+
+spe <- paste0(
+  meta[["resources_dir"]],
+  "/xenium_tiny"
+)
+out_rds <- "output.rds"
+
+cat("> Running ", meta[["name"]], "\n", sep = "")
+out <- processx::run(
+  meta[["executable"]],
+  c(
+    "--input", spe,
+    "--output", out_rds
+  )
+)
+
+cat("> Checking whether output file exists\n")
+expect_equal(out$status, 0)
+expect_true(file.exists(out_rds))
+
+cat("> Reading output file\n")
+obj <- readRDS(file = out_rds)
+
+cat("> Checking whether Seurat object is in the right format\n")
+# Object type
+expect_is(obj, "SpatialExperiment")
+# Assay structure
+expect_equal(names(slot(obj, "assays")), "counts")
+# Spatial coordinates
+expect_equal(spatialCoordsNames(obj), c("x_centroid", "y_centroid"))
+# Alternative experiments
+expect_equal(altExpNames(obj), c("NegControlProbe", "UnassignedCodeword", "NegControlCodeword"))
+# Metadata components
+metadata_components <- c("experiment.xenium", "transcripts", "cell_boundaries", "nucleus_boundaries")
+expect_named(
+  metadata(obj),
+  metadata_components,
+  ignore.order = TRUE
+)
+# Parquet paths
+parquet_components <- c("transcripts", "cell_boundaries", "nucleus_boundaries")
+for (component in parquet_components) {
+  expect_true(grepl("\\.parquet$", metadata(obj)[[component]]))
+}
+# Dimensions
+input <- readXeniumSXE(
+  dirName = spe,
+  returnType = "SPE"
+)
+dim_rds <- dim(obj)
+dim_input <- dim(input)
+
+expect_equal(dim_rds, dim_input)
+
+
+cat("> Checking parameter functionality\n")
+
+out_rds_ext <- "output_ext.rds"
+
+cat("> Running ", meta[["name"]], "\n", sep = "")
+out_ext <- processx::run(
+  meta[["executable"]],
+  c(
+    "--input", spe,
+    "--add_experiment_xenium", FALSE,
+    "--add_parquet_paths", FALSE,
+    "--alternative_experiment_features", c("NegControlProbe"),
+    "--output", out_rds_ext
+  )
+)
+
+cat("> Checking whether output file exists\n")
+expect_equal(out_ext$status, 0)
+expect_true(file.exists(out_rds_ext))
+
+cat("> Reading output file\n")
+obj_ext <- readRDS(file = out_rds_ext)
+
+cat("> Checking whether Seurat object is in the right format\n")
+# Object type
+expect_is(obj_ext, "SpatialExperiment")
+# Assay structure
+expect_equal(names(slot(obj_ext, "assays")), "counts")
+# Spatial coordinates
+expect_equal(spatialCoordsNames(obj_ext), c("x_centroid", "y_centroid"))
+# Alternative experiments
+expect_equal(altExpNames(obj_ext), c("NegControlProbe"))
+# Metadata components
+expect_true(length(metadata(obj_ext)) == 0)
+
+dim_rds_ext <- dim(obj_ext)
+expect_true(identical(dim_rds_ext[2], dim_input[2]))
+expect_false(identical(dim_rds_ext[1], dim_input[1]))
