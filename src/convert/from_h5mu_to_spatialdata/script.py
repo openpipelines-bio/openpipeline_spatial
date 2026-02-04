5ecf35247d607bd044ad904a7846c8f65e9943b4
Luke Zappia <luke@data-intuitive.com>
2026-02-04 16:01:36 +0100

    Add input_spatialdata to from_h5mu_to_spatialdata

--- src/convert/from_h5mu_to_spatialdata/script.py
+++ src/convert/from_h5mu_to_spatialdata/script.py
@@ -1,11 +1,13 @@
 import sys
 import spatialdata as sd
 import mudata as mu
+import logging
 
 ## VIASH START
 par = {
     "input": "./resources_test/xenium/xenium_tiny.h5mu",
-    "output": "./resources_test/xenium/xenium_tiny.zarr",
+    "input_spatialdata": "./resources_test/xenium/xenium_tiny.zarr",
+    "output": "./resources_test/xenium/xenium_tiny_from_h5mu.zarr",
     "modality": "rna",
 }
 meta = {"resources_dir": "src/utils"}
@@ -27,8 +29,32 @@ if ("spatialdata_attrs" in mod.uns):
     logger.info("Removing existing SpatialData attributes from modality...")
     del mod.uns["spatialdata_attrs"]
 
+if (par.get("input_spatialdata", None) is not None):
+    logger.info(f"Reading existing SpatialData from {par['input_spatialdata']}...")
+    
+    # Disable logger messages from spatialdata when reading
+    logger.setLevel(logging.WARNING)
+    sdata_existing = sd.read_zarr(par["input_spatialdata"])
+    logger.setLevel(logging.INFO)
+
+    logger.info("Checking modality matches existing SpatialData table...")
+    if not mod.n_obs == sdata_existing["table"].n_obs:
+        raise ValueError(
+            "The number of observations in the selected modality does not match the existing SpatialData table."
+        )
+    if not mod.obs_names.equals(sdata_existing["table"].obs_names):
+        raise ValueError(
+            "The observation names in the selected modality do not match the existing SpatialData table."
+        )
+
 logger.info("Creating SpatialData object...")
-sdata = sd.SpatialData(tables={"table": mod})
+if (par.get("input_spatialdata", None) is not None):
+    logger.info("Using existing SpatialData...")
+    sdata = sdata_existing
+    sdata["table"] = mod
+else:
+    logger.info("Creating new SpatialData...")
+    sdata = sd.SpatialData(tables={"table": mod})
 
 logger.info(f"Writing output SpatialData Zarr store to {par['output']}...")
 sdata.write(par["output"], overwrite=True)
