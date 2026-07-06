21c230809491f1457604d604ccda98a1adefd697
Jakub Majercik <jakub@data-intuitive.com>
2026-07-06 10:25:29 +0200

    Document mm10 tiny reference source and build recipe

--- resources_test_scripts/reference_mm10_tiny.sh
+++ resources_test_scripts/reference_mm10_tiny.sh
@@ -5,11 +5,28 @@ set -eo pipefail
 # Tiny mm10 Space Ranger reference for the Visium HD ingestion test, kept as a
 # pre-staged artifact on S3 (same approach as reference_tiny.sh for GRCh38).
 #
-# The artifact was built by subsetting the official 10x "mm10-2020-A" reference
-# to chr19 (smallest mouse autosome) + chrM and rebuilding the reference package
-# with (genome name must stay "mm10" to match the mm10-2020-A mouse probe set):
+# Source: the official 10x Genomics "mm10-2020-A" gene expression reference
+#   https://cf.10xgenomics.com/supp/cell-exp/refdata-gex-mm10-2020-A.tar.gz
 #
-#   spaceranger mkref --genome=mm10 --fasta=tiny.fa --genes=tiny.gtf --nthreads=4 --memgb=6
+# The artifact was built by subsetting that reference to chr19 (smallest mouse
+# autosome) + chrM and rebuilding the reference package. The genome name must
+# stay "mm10" to match the mm10-2020-A mouse probe set. Reproduction recipe:
+#
+#   # Download and unpack the full 10x mm10-2020-A reference
+#   curl -fSL -o refdata-gex-mm10-2020-A.tar.gz \
+#     https://cf.10xgenomics.com/supp/cell-exp/refdata-gex-mm10-2020-A.tar.gz
+#   tar xzf refdata-gex-mm10-2020-A.tar.gz
+#
+#   # Subset the genome FASTA to chr19 + chrM
+#   samtools faidx refdata-gex-mm10-2020-A/fasta/genome.fa chr19 chrM > tiny.fa
+#
+#   # Subset the gene annotation to the same contigs (keep header lines)
+#   awk '/^#/ || $1 == "chr19" || $1 == "chrM"' \
+#     refdata-gex-mm10-2020-A/genes/genes.gtf > tiny.gtf
+#
+#   # Rebuild the reference package (produces ./mm10)
+#   spaceranger mkref --genome=mm10 --fasta=tiny.fa --genes=tiny.gtf \
+#     --nthreads=4 --memgb=6
 #
 # TODO: replace this manual mkref step with a `viash run` of a spaceranger_mkref
 # component (to be implemented), so the reference can be rebuilt here directly.
